7490f13c44dd4d3091c8a3f90e94f107858411b4
Rich Wareham <rich@richwareham.com>
2016-03-13 11:08:23 +0000

    Add simple LCD test code

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,4 +1,12 @@
+#include <LiquidCrystal.h>
+
 #include "AnalogWrite16.h"
+#include "pins.h"
+
+// Liquid crystal display
+LiquidCrystal lcd(
+    PIN_LCD_RS, PIN_LCD_E, PIN_LCD_D4, PIN_LCD_D5, PIN_LCD_D6, PIN_LCD_D7
+);
 
 /* Note on "analog" output
  * =======================
@@ -25,35 +33,62 @@
  * means we should be able to feed PWM directly into the control board.
  */
 
-const int PIN_VOUT_SET = 9;
-const int PIN_IOUT_SET = 10;
 
-const float SUPPLY_VOLTAGE = 5.f - 0.4f; // Minus 400mV from BAT85 diode
+const float PWM_P_TO_P = 3.3f; // We buffer through a 4050
+const uint16_t MAX_ANALOG_WRITE = 0x03ff; // 10-bit PWM
 
 // TODO: These formulae are taken directly from the reverse engineering of the
 // B3603. They need verifying.
 
 inline uint16_t v_out_to_pwm(float v_out) {
     float target_voltage = (16.f/15.f) * (31e-3f + 0.068f*v_out);
-    float duty_cycle = max(0.f, min(1.f, target_voltage / SUPPLY_VOLTAGE));
-    return static_cast<uint16_t>(duty_cycle * 65535.f);
+    float duty_cycle = max(0.f, min(1.f, target_voltage / PWM_P_TO_P));
+    return static_cast<uint16_t>(duty_cycle * MAX_ANALOG_WRITE);
 }
 
 inline uint16_t i_out_to_pwm(float i_out) {
     float target_voltage = 16.f * (10e-3f + 0.05f*i_out);
-    float duty_cycle = max(0.f, min(1.f, target_voltage / SUPPLY_VOLTAGE));
-    return static_cast<uint16_t>(duty_cycle * 65535.f);
+    //float target_voltage = 0.97f*i_out + 0.14f;
+    float duty_cycle = max(0.f, min(1.f, target_voltage / PWM_P_TO_P));
+    return static_cast<uint16_t>(duty_cycle * MAX_ANALOG_WRITE);
 }
+
+/* See:
+ * http://arduino.stackexchange.com/questions/12718/increase-pwm-bit-resolution
+ */
+
+/* Configure digital pins 9 and 10 as 16-bit PWM outputs. */
+inline void setupHiPWM() {
+    DDRB |= _BV(PB1) | _BV(PB2);        /* set pins as outputs */
+    TCCR1A = _BV(COM1A1) | _BV(COM1B1)  /* non-inverting PWM */
+        | _BV(WGM11) | _BV(WGM10);      /* mode 14: fast PWM, TOP=0x03ff */
+    TCCR1B = _BV(WGM12)
+        | _BV(CS10);                    /* no prescaling */
+}
+
+/* High precision version of analogWrite(). Works only on pins 9 and 10. */
+inline void analogWriteHi(uint8_t pin, uint16_t val)
+{
+    switch (pin) {
+        case  9: OCR1A = val; break;
+        case 10: OCR1B = val; break;
+    }
+}
+
 
 void setup() {
     // Enable high-precision PWM output. We use this to allow for 16-bit control
     // of the current and voltage setting without resorting to tricks such as
     // voltage summing two 8-bit pins, etc.
-    setupPWM16();
+    setupHiPWM();
+
+    // Configure LCD display
+    lcd.begin(16, 2);
+    lcd.print("I'm alive!");
 
     // Test
-    analogWrite16(PIN_IOUT_SET, i_out_to_pwm(20e-3));
-    analogWrite16(PIN_VOUT_SET, v_out_to_pwm(5.0));
+    analogWrite16(PIN_IOUT_SET, i_out_to_pwm(0.3));
+    analogWrite16(PIN_VOUT_SET, v_out_to_pwm(3.3));
 }
 
 void loop() {
---
+++ firmware/pins.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef PINS__H
+#define PINS__H
+
+// Pins for rotary encoder switch
+const int PIN_RE_SW = 6;
+const int PIN_RE_CLK = 7;
+const int PIN_RW_DT = 8;
+
+// Pins for LCD module
+const int PIN_LCD_RS = A0;
+const int PIN_LCD_E = A1;
+const int PIN_LCD_D4 = 2;
+const int PIN_LCD_D5 = 3;
+const int PIN_LCD_D6 = 4;
+const int PIN_LCD_D7 = 5;
+
+// Output pins to D3806 module
+const int PIN_VOUT_SET = 9;
+const int PIN_IOUT_SET = 10;
+const int PIN_NOT_ON = A7;
+
+// Input pins from D3806
+const int PIN_CC_CV = A5;
+const int PIN_IOUT_SENSE = A6;
+const int PIN_VOUT_SENSE = A7;
+
+#endif // PINS__H
